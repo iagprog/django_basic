8a936cf74424a72fc5db6fafbe397f837579f74c
iagprog <lgsoftlab@gmail.com>
2020-02-13 08:46:46 -0800

    main update: * add media files support (settings.py; urls.py - DEBUG mode) * add models to models.py * add superuser for admin access * add content to sqlite DB * fix templates to work with models

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 import mainapp.views as mainapp
+
 
 urlpatterns = [
     path( '' , mainapp.main, name = "main"),
@@ -29,3 +32,5 @@ urlpatterns = [
     path( 'products/products_classic' , mainapp.products, name = "products_classic"),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
