9dbe53c5163b25e138a39cb0da2c0dd47da2ffe5
iagprog <lgsoftlab@gmail.com>
2020-02-13 14:52:46 -0800

    stable version

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -4,42 +4,17 @@ from .models import Product, Contact, ProductCategory, ContactCountry
 # Create your views here.
 def main(request):
     title = "Home"
-    excl_main = Product.objects.all()[:2]
-    content = {"title": title, "excl_main": excl_main}
+    excl = Product.objects.all()[:2]
+    content = {"title": title, "excl": excl}
     return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
     title = "Products"
-    links_menu = [
-        {
-            "href": "products_all",
-            "name": "all"
-        },
-        {
-            "href": "products_home",
-            "name": "home"
-        },
-        {
-            "href": "products_office",
-            "name": "office"
-        },
-        {
-            "href": "products_furniture",
-            "name": "furniture"
-        },
-        {
-            "href": "products_modern",
-            "name": "modern"
-        },
-        {
-            "href": "products_classic",
-            "name": "classic"
-        }
-    ]
-    excl_products = Product.objects.all()[:2]
+    excl = Product.objects.all()[:2]
     products = Product.objects.all()[2:]
-    content = {"title": title, "links_menu": links_menu, "products": products, "excl_products": excl_products}
+    categories = ProductCategory.objects.all()
+    content = {"title": title, "excl": excl, "categories": categories, "products": products}
     return render(request, 'mainapp/products.html', content)
 
 
@@ -50,21 +25,25 @@ def contact(request):
     return render(request, 'mainapp/contact.html', content)
 
 
-def products_all(request):
+def all(request):
     return render(request, 'mainapp/products.html')
 
 
-def products_home(request):
+def home(request):
     return render(request, 'mainapp/products.html')
 
 
-def products_office(request):
+def office(request):
     return render(request, 'mainapp/products.html')
 
 
-def products_furniture(request):
+def furniture(request):
     return render(request, 'mainapp/products.html')
 
 
-def products_modern(request):
+def modern(request):
+    return render(request, 'mainapp/products.html')
+
+
+def exclusive(request):
     return render(request, 'mainapp/products.html')
