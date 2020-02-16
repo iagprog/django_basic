0e7b68a37af85c5e94d5643c76d9681eb5e547c7
iagprog <lgsoftlab@gmail.com>
2020-02-16 10:58:49 -0800

    authapp main update: *  add urls.py *  add login/logout views in views.py * add forms.py: ShopUserLoginForm * add templates: base.html, login.html * fix inc_menu to see auth. process

--- geekshop/authapp/urls.py
+++ geekshop/authapp/urls.py
@@ -6,4 +6,6 @@ app_name = 'authapp'
 urlpatterns = [
     path('login/' , authapp.login, name= 'login'),
     path('logout/' , authapp.logout, name= 'logout'),
+    path('edit/' , authapp.logout, name= 'edit'),
+    path('register/' , authapp.logout, name= 'register'),
 ]
