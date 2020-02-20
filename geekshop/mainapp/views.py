0eddabe2f46628a36820ffe30641981903484122
iagprog <lgsoftlab@gmail.com>
2020-02-20 06:41:44 -0800

    complete products destr by categories

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -4,16 +4,19 @@ from .models import Product, Contact, ProductCategory, ContactCountry
 # Create your views here.
 def main(request):
     title = "Home"
-    excl = Product.objects.all()[12:]
+    excl = Product.objects.filter(category__name = 'exclusive')
     content = {"title": title, "excl": excl}
     return render(request, 'mainapp/index.html', content)
 
 
 def products(request, pk=None):
     title = "Products"
-    excl = Product.objects.all()[12:]
-    products = Product.objects.all()[:12]
-    categories = ProductCategory.objects.all()[:6]
+    excl = Product.objects.filter(category__name = 'exclusive')
+    if pk == 1 or pk is None:  # category = 'all'
+        products = Product.objects.exclude(category__name = 'exclusive')
+    else:
+        products = Product.objects.filter(category__pk = pk)
+    categories = ProductCategory.objects.exclude(name = 'exclusive')
     content = {"title": title, "excl": excl, "categories": categories, "products": products, "pk":pk}
     return render(request, 'mainapp/products.html', content)
 
