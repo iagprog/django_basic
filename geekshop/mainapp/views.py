9e3274a3dd9b04a6ad8ef763d6c1fa6d20b16f27
iagprog <lgsoftlab@gmail.com>
2020-02-20 11:17:37 -0800

    big update: * basket: urls + model + controller * 'add product to basket' function + counter

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product, Contact, ProductCategory, ContactCountry
+from basketapp.models import Basket 
 
 # Create your views here.
 def main(request):
@@ -11,13 +12,16 @@ def main(request):
 
 def products(request, pk=None):
     title = "Products"
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
     excl = Product.objects.filter(category__name = 'exclusive')
     if pk == 1 or pk is None:  # category = 'all'
         products = Product.objects.exclude(category__name = 'exclusive')
     else:
         products = Product.objects.filter(category__pk = pk)
     categories = ProductCategory.objects.exclude(name = 'exclusive')
-    content = {"title": title, "excl": excl, "categories": categories, "products": products, "pk":pk}
+    content = {"title": title, "excl": excl, "categories": categories, "products": products, "basket": basket, "pk":pk}
     return render(request, 'mainapp/products.html', content)
 
 
