7ccfbccf107cdfc24450e6dac7422c96eac51eee
iagprog <lgsoftlab@gmail.com>
2020-02-25 14:39:18 -0800

    * add @login_required * add reverse to 'showroom' after logon

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 
 def login(request):
     title = 'login'
-
+    next = request.GET['next'] if 'next' in request.GET.keys() else ''
     login_form = ShopUserLoginForm(data=request.POST)
     if request.method == 'POST' and login_form.is_valid():
         username = request.POST['username']
@@ -16,9 +16,12 @@ def login(request):
         user = auth.authenticate(username=username, password=password)
         if user and user.is_active:
             auth.login(request, user)
-            return HttpResponseRedirect(reverse('main'))
+            if 'next' in request.POST.keys():
+                return HttpResponseRedirect(request.POST['next'])
+            else:
+                return HttpResponseRedirect(reverse('main'))
 
-    content = {'title' : title, 'login_form' : login_form}
+    content = {'title' : title, 'login_form' : login_form, 'next' : next}
     return render(request, 'authapp/login.html' , content)
 
 def logout(request):
--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 from mainapp.models import Product
 from basketapp.models import Basket
 
 # Create your views here.
-  
+
+@login_required
 def basket(request):
     title = "basket"
     basket_items = Basket.objects.filter(user=request.user).order_by("product__category")
@@ -13,6 +16,7 @@ def basket(request):
     return render(request, "basketapp/basket.html", content)
 
 
+@login_required
 def basket_add(request, pk):
     product = get_object_or_404(Product, pk=pk)
     basket = Basket.objects.filter(user=request.user, product=product).first()
@@ -22,10 +26,12 @@ def basket_add(request, pk):
 
     basket.quantity += 1
     basket.save()
-
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:showroom' , args=[pk]))
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
+@login_required
 def basket_remove(request, pk):
     basket_record = get_object_or_404(Basket, pk=pk)
     basket_record.delete()
